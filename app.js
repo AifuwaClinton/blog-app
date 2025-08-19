c080b6c809b34ec15c001a69a785847243374986
Aifuwa Clinton <clintonaifuwa7@gmail.com>
2025-08-19 07:16:48 -0700

    done with today class

---
+++ app.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const express = require('express')
+const expressLayout = require('express-ejs-layouts');
+const router = require('./routes/main');
+const connectDB = require('./utils/db');
+const app = express();
+
+const PORT = 4000 || process.env.PORT;
+connectDB();
+
+app.use(express.static('public'));
+//Template Engine
+app.use(expressLayout)
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+app.use("", router);
+
+
+
+
+app.listen(PORT, (req,res) => {
+  console.log(`app is listening on http://localhost:${PORT}`);
+});
+
